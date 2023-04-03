0dc0437044d1a307475461f3f0e40d43cb5fbc3f
Paul Boersma <pcboersma91@gmail.com>
2023-04-03 21:23:59 +0200

    Update SubscriptionCleanup.ps1
    Formating

--- SubscriptionCleanup.ps1
+++ SubscriptionCleanup.ps1
@@ -3,34 +3,34 @@ Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Subscription Cleaup' 
-$form.Size = New-Object System.Drawing.Size(300,800)
+$form.Size = New-Object System.Drawing.Size(300, 800)
 $form.StartPosition = 'CenterScreen'
 
 $okButton = New-Object System.Windows.Forms.Button
-$okButton.Location = New-Object System.Drawing.Point(75,700)
-$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Location = New-Object System.Drawing.Point(75, 700)
+$okButton.Size = New-Object System.Drawing.Size(75, 23)
 $okButton.Text = 'OK'
 $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $form.AcceptButton = $okButton
 $form.Controls.Add($okButton)
 
 $cancelButton = New-Object System.Windows.Forms.Button
-$cancelButton.Location = New-Object System.Drawing.Point(150,700)
-$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Location = New-Object System.Drawing.Point(150, 700)
+$cancelButton.Size = New-Object System.Drawing.Size(75, 23)
 $cancelButton.Text = 'Cancel'
 $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
 $form.CancelButton = $cancelButton
 $form.Controls.Add($cancelButton)
 
 $label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(10,20)
-$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Location = New-Object System.Drawing.Point(10, 20)
+$label.Size = New-Object System.Drawing.Size(280, 20)
 $label.Text = 'Subscription to bulk remove resource groups:'
 $form.Controls.Add($label)
 
 $listBox = New-Object System.Windows.Forms.ListBox
-$listBox.Location = New-Object System.Drawing.Point(10,40)
-$listBox.Size = New-Object System.Drawing.Size(260,600)
+$listBox.Location = New-Object System.Drawing.Point(10, 40)
+$listBox.Size = New-Object System.Drawing.Size(260, 600)
 $listBox.Height = 600
 
 # Get subscriptions for the current tenant
@@ -42,8 +42,7 @@ $form.Controls.Add($listBox)
 $form.Topmost = $true
 $result = $form.ShowDialog()
 
-if ($result -eq [System.Windows.Forms.DialogResult]::OK)
-{
+if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
     $activeSubscription = Set-AzContext $listBox.SelectedItem
     Write-Host 'Active Subscription: '
     $activeSubscription
